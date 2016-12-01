96889da889409b99fd00b845bfc70aa394fd416f
2devnull <2devnull@users.noreply.github.com>
2016-12-01 16:10:40 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ touch /var/log/spiped
 ssh -oStrictHostKeyChecking=no -f -N -D 0.0.0.0:1080 localhost
 /spiped/spiped/spiped -g -n 500 -d -s '[0.0.0.0]:8089' -t '[127.0.0.1]:1080' -k  /spipedkey/spiped.key
 
-tail -f /var/log/spiped
+# tail -f /var/log/spiped
